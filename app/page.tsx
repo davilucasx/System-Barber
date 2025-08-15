32453ccb52ed03efd4533427bdafcbe47b772a7d
davilucasx <lucasdavimarinho9@gmail.com>
2025-08-15 19:31:25 -0300

    chore: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/app/_components/ui/button"; 
+
 const Home = () => {
-  return ( <h1 className="text-red-300 ">Page main</h1> );
+  return <Button>teste de componente</Button>
 }
  
 export default Home;
